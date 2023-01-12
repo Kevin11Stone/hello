2b92dafa57a3c09c82d28385daa405a2c45e43e6
Kevin Stone <ruca6616@gmail.com>
2023-01-12 17:13:19 -0500

    added new view and updated index to route to home.html

--- index.php
+++ index.php
@@ -9,14 +9,18 @@ error_reporting(E_ALL);
 // require the autoload file
 require_once('vendor/autoload.php');
 
-// create an instance of the Base class
+// instance (from object name) vs. static (from class name)
+// create an instance of the Base class, f3 = fat-free-framework
 $f3 = Base::instance();
 // Java equivalent -> Base f3 = new Base();
 
 
 // define a default route ("home page")
 $f3->route('GET /', function (){
-   echo '<h1>Hello, Fat Free!</h1>';
+   //echo '<h1>Hello, Fat Free!</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
+
 });
 
 
